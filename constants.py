48f9f25a4b8f094dcffe8ddf63daed5ae1874bda
apireno <139802728+apireno@users.noreply.github.com>
2025-01-06 18:40:30 +0100

    Add files via upload

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ from database import Database
 
 
 class Constants():
-    DB_URL = "wss://test-med-069pe77i4lv4j3mhm6bc7t5u9g.aws-use1.surreal.cloud"
+    DB_URL = "wss://xxxx"
     DB_NAMESPACE = "embedding_example"
     DB_DATABASE = "embedding_example"
     DB_PARAMS = Database(DB_URL,DB_NAMESPACE,DB_DATABASE)
@@ -10,6 +10,8 @@ class Constants():
 
     THIS_FOLDER = "./"
     PREV_EXTRACTED_INGREDIENTS_FILE = THIS_FOLDER + "/extracted_ingredients_list.txt"
-    MODEL_PATH = THIS_FOLDER + "/glove.6B.50d.txt"
-    
+    #You must download a model in the format of:
+    # word v1 v2 v3 ... vN
+    #here is an example
     #https://www.kaggle.com/datasets/watts2/glove6b50dtxt
+    MODEL_PATH = THIS_FOLDER + "/glove.6B.50d.txt"
