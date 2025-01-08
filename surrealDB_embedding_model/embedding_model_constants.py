db8516325219f4bc91daff4716646fcbdf3b1795
Alessandro Pireno <sandro@pireno.com>
2025-01-07 20:50:15 -0500

    changed args parsing

--- surrealDB_embedding_model/embedding_model_constants.py
+++ surrealDB_embedding_model/embedding_model_constants.py
@@ -26,17 +26,16 @@ class DatabaseConstants():
         #For use in authenticating your database in database.py
         #These are just the pointers to the environment variables
         #Don't put the actual passwords here
-
-    def LoadArgs(self,description):
-        parser = argparse.ArgumentParser(description=description)
+    def AddArgs(self, parser:argparse.ArgumentParser):
+        
         parser.add_argument("-url","--url", help="Path to your SurrealDB instance (Default: {0})".format(self.DB_PARAMS.url))
-        parser.add_argument("-db","--database", help="SurrealDB database to create and install the model (Default: {0})".format(self.DB_PARAMS.database))
-        parser.add_argument("-mp","--model_path", help="Your model file (Default: {0})".format(self.MODEL_PATH))
+        parser.add_argument("-ns","--namespace", help="SurrealDB namespace to create and install the data (Default: {0})".format(self.DB_PARAMS.database))
+        parser.add_argument("-db","--database", help="SurrealDB database to create and install the data (Default: {0})".format(self.DB_PARAMS.namespace))
         parser.add_argument("-uenv","--user_env", help="Your environment variable for db username (Default: {0})".format(self.DB_USER_ENV_VAR))
         parser.add_argument("-penv","--pass_env", help="Your environment variable for db password (Default: {0})".format(self.DB_PASS_ENV_VAR))
         
-        args = parser.parse_args()
-
+    def SetArgs(self,args:argparse.Namespace):
+        
         if args.url:
             self.DB_PARAMS.url = args.url
         if args.namespace:
@@ -50,62 +49,37 @@ class DatabaseConstants():
             self.DB_PASS_ENV_VAR = args.pass_env
             self.DB_PARAMS.password = os.getenv(self.DB_PASS_ENV_VAR)
 
-        
-
     
 
 
 class EmbeddingModelConstants():
     def __init__(self):
-        
         self.MODEL_PATH = THIS_FOLDER + "data/glove.6B.50d.txt"
-
-    def LoadArgs(self,description):
-
-        parser = argparse.ArgumentParser(description=description)
+    def AddArgs(self, parser:argparse.ArgumentParser):
         parser.add_argument("-mp","--model_path", help="Your model file (Default: {0})".format(self.MODEL_PATH))
-        
-        args = parser.parse_args()
-
+    def SetArgs(self,args:argparse.Namespace):
         if args.model_path:
             self.MODEL_PATH = args.model_path
-       
-
         
+    
 
+    
     
 
 class ArgsLoader():
-    @staticmethod
-    def LoadArgs(
-            description,
+
+    def __init__(self,description):
+        self.parser = argparse.ArgumentParser(description=description)
+
+    def LoadArgs(self,
             db_constants: DatabaseConstants,
             embed_constants: EmbeddingModelConstants
             ):
-        
+        embed_constants.AddArgs(self.parser)
+        db_constants.AddArgs(self.parser)
 
-        parser = argparse.ArgumentParser(description=description)
-        parser.add_argument("-url","--url", help="Path to your SurrealDB instance (Default: {0})".format(db_constants.DB_PARAMS.url))
-        parser.add_argument("-ns","--namespace", help="SurrealDB namespace to create and install the data (Default: {0})".format(db_constants.DB_PARAMS.database))
-        parser.add_argument("-db","--database", help="SurrealDB database to create and install the data (Default: {0})".format(db_constants.DB_PARAMS.namespace))
-        parser.add_argument("-uenv","--user_env", help="Your environment variable for db username (Default: {0})".format(db_constants.DB_USER_ENV_VAR))
-        parser.add_argument("-penv","--pass_env", help="Your environment variable for db password (Default: {0})".format(db_constants.DB_PASS_ENV_VAR))
-        parser.add_argument("-mp","--model_path", help="Your model file (Default: {0})".format(embed_constants.MODEL_PATH))
-        
-        args = parser.parse_args()
+        args = self.parser.parse_args()
 
-        if args.url:
-            db_constants.DB_PARAMS.url = args.url
-        if args.namespace:
-            db_constants.DB_PARAMS.namespace = args.namespace
-        if args.database:
-            db_constants.DB_PARAMS.database = args.database
-        if args.user_env:
-            db_constants.DB_USER_ENV_VAR = args.user_env
-            db_constants.DB_PARAMS.username = os.getenv(db_constants.DB_USER_ENV_VAR)
-        if args.pass_env:
-            db_constants.DB_PASS_ENV_VAR = args.pass_env
-            db_constants.DB_PARAMS.password = os.getenv(db_constants.DB_PASS_ENV_VAR)
-        if args.model_path:
-            embed_constants.MODEL_PATH = args.model_path
+        embed_constants.SetArgs(args)
+        db_constants.SetArgs(args)
         
